3613dcc97a0c6fad3bb28611eb744a63be8867e8
luzanov99 <luzanov.zena@gmail.com>
2022-08-30 15:04:05 +0300

    add databases conections

---
+++ db/elastic.py
@@ -0,0 +1,6 @@
+from elasticsearch import AsyncElasticsearch
+
+es: AsyncElasticsearch = None
+
+async def get_elastic() -> AsyncElasticsearch:
+    return es
--- main.py
+++ main.py
@@ -4,16 +4,26 @@ import core.config as config
 from typing import Union
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
-
 from core.logger import LOGGING
-
+from db import redis
+from db import elastic
+from elasticsearch import AsyncElasticsearch
+import aioredis
 app = FastAPI(
     title=config.PROJECT_NAME,
     docs_url='/api/openapi',
     openapi_url='/api/openapi.json',
     default_response_class=ORJSONResponse,
 )
+@app.on_event('startup')
+async def startup():
+    redis.redis = await aioredis.create_redis_pool((config.REDIS_HOST, config.REDIS_PORT), minsize=10,maxsize=20)
+    elastic.es = AsyncElasticsearch(hosts=[f'{config.ELASTIC_HOST}: {config.ELASTIC_PORT}'])
 
+@app.on_event('shutdown')
+async def shutdown():
+    await redis.redis.close()
+    await elastic.es.close()
 
 
 @app.get('/items/{item_id}/')
